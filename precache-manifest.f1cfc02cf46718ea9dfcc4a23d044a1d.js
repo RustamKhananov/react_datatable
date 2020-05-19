ce54d502189de9fcced051bedec5afc513174e85
Rustam Khananov <antmine2017@gmail.com>
2020-05-19 12:06:12 +0300

    Updates

---
+++ precache-manifest.f1cfc02cf46718ea9dfcc4a23d044a1d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5391448c633e634676acc2b72962b220",
+    "url": "/react_datatable/index.html"
+  },
+  {
+    "revision": "4dd5c2515231c81af492",
+    "url": "/react_datatable/static/js/2.20add5bd.chunk.js"
+  },
+  {
+    "revision": "941e8c918c8720249555",
+    "url": "/react_datatable/static/js/main.f3d5c3a2.chunk.js"
+  },
+  {
+    "revision": "cd26d19e335861885027",
+    "url": "/react_datatable/static/js/runtime~main.ce12b3cb.js"
+  }
+]);
